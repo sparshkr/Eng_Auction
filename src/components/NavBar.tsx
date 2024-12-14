d232ab5c133d41ff64ce57ea8a2be987640463b4
sparshkr <sparshkumar910@gmail.com>
2024-12-14 12:02:04 +0530

    Styling-12

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -31,13 +31,13 @@ export default function NavBar(): JSX.Element {
   ];
 
   return (
-    <div className="sticky bottom-16 md:w-[105%] w-[90%] border-2 py-1 px-2 m-0 rounded-[54.88px] h-[94.97px] z-50 bg-[#b7b0b0] bg-opacity-10 text-white border-1 border-[#DFDADA17]  md:bottom-5 md:left-2 md:h-[50px] mu:h-[70px] pb-1 mu:fixed ms:bottom-3 mu:bottom-4">
-      <div className="h-full w-full m-0 pt-2 flex justify-around items-center pb-1">
+    <div className="sticky bottom-16 md:w-[105%] w-[90%] border-2 py-2 px-2 m-0 rounded-[54.88px] h-[94.97px] z-50 bg-[#b7b0b0] bg-opacity-10 text-white border-1 border-[#DFDADA17]  md:bottom-5 md:left-2 md:h-[55px] mu:h-[70px] pb-1 mu:fixed ms:bottom-3 mu:bottom-4">
+      <div className="h-full w-full m-0  flex justify-around items-center">
         {navItems.map((navItem, index) => (
           <button
             key={index}
             className={`flex flex-col items-center ${
-              index === 0 ? "gap-[7px]" : "gap-0"
+              index === 0 ? "gap-[2px]" : "gap-0"
             }`}
           >
             <div className="h-10 w-[30px] md:h-6 md:w-5 transition-all duration-300 ease-in-out hover:scale-110 mu:h-9">
@@ -46,7 +46,7 @@ export default function NavBar(): JSX.Element {
                 alt={navItem.name}
                 width={32}
                 height={32}
-                className=""
+                className={`${navItem.name == "How to" ? `mt-[4px]` : `mt-0`}`}
               />
             </div>
             <span className="text-sm md:text-[10px] mu:text-[14px]">
