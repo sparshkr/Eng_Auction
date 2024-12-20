c2c206e1269237b0660c9d763de48627514c7e56
Vedant-asati <22je1064@iitism.ac.in>
2024-12-20 17:13:29 +0530

    fix env

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000';
 
 export const ROUTES = {
   AUTH: {
